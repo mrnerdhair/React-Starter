59f9e3f7511decb3f0dfa40c67d666a164750d1f
buckoon <jeremy8@vt.edu>
2022-07-08 16:44:25 -0400

    need help getting oracle logo to appear

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react"
+import Oracleimage from "./Oracleimage"
 
 
 
 export default function App() {
-    const [motorState, setMotorState] = React.useState("AN");
-    }
+    const [motorState, setMotorState] = React.useState("");
+ 
+
+
+   
 
     return (
     
-<div className ="motor">
+
+
+ <div>
+   <Oracleimage/>
+     </div>
+  
+ <div className ="motor">
     <select 
     className="custom-select" 
     value={motorState}
@@ -17,13 +27,45 @@ export default function App() {
     setMotorState(selectedMotor);
     }}
    >
-    <option value="AC">AC for International std Mount</option>
+        <option value="AC">AC for International std Mount</option>
         <option value="AN">AN International Std Mount</option>
         <option value="CC">CC for alt std mount, closed key</option>
-        <select/>
+    </select>
         {motorState}
-       </ div>
-)
+
+
+
+    <select 
+    className="custom-select" 
+    value={motorState}
+    onChange={(e)=>{
+    const selectedMotor=e.target.value;
+    setMotorState(selectedMotor);
+    }}
+   >
+        <option value="AC">AC for International std Mount</option>
+        <option value="AN">AN International Std Mount</option>
+        <option value="CC">CC for alt std mount, closed key</option>
+    </select>
+        {motorState}
+
+        
+    <select 
+    className="custom-select" 
+    value={motorState}
+    onChange={(e)=>{
+    const selectedMotor=e.target.value;
+    setMotorState(selectedMotor);
+    }}
+   >
+        <option value="cccccccccccccc">AC for International std Mount</option>
+        <option value="AN">AN International Std Mount</option>
+        <option value="CC">CC for alt std mount, closed key</option>
+    </select>
+        {motorState}
+  </div>
+    )
+}
      
    
 
---
+++ src/Oracleimage.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./images/Oracle.png"
+export default function Oracleimage() {
+
+
+
+
+    
+
+        return(
+
+            
+            <div>
+            <img src= {logo} alt=""  className="hero--photo"/> 
+
+            
+           
+            
+            </div>
+            )
+    
+}
+
+
+
